291723f21379799fb7252424816f02f2d674b6ec
MeowB <60074375+MeowB@users.noreply.github.com>
2025-02-01 21:41:10 +0100

    routing and proxy

--- src/Pages/JobsPage.tsx
+++ src/Pages/JobsPage.tsx
@@ -1,9 +1,10 @@
-import jobs from '../jobs.json'
+import JobListings from "../components/JobListings"
+
 const JobsPage = () => {
   return (
-	<>
-	  
-	</>
+	  <section className='bg-blue-50 px-4 py-6'>
+		<JobListings />
+	  </section>
   )
 }
 
